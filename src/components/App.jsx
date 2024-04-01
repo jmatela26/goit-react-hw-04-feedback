cb000a0844decda4640eec226455296e8fa829d1
jmatela26 <jmatela26@gmail.com>
2024-04-01 23:32:24 +0800

    Added react hooks comment

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ import { Notification } from './Notification/Notification';
 import { useState } from 'react';
 
 export const App = () => {
+  // Added react hooks
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
